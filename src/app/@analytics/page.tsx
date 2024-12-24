a08a99b1955223889a5c8360be4cae1ce52d8009
Jecheon Ryu <rjc1704@gmail.com>
2024-12-24 10:14:54 +0900

    에러 격리

--- src/app/@analytics/page.tsx
+++ src/app/@analytics/page.tsx
@@ -1,3 +1,7 @@
-export default function AnalyticsPage() {
+export default async function AnalyticsPage() {
+  //   const res = await fetch("abc");
+  //   if (!res.ok) throw new Error("애널리틱스 페이지 에러발생");
   return <div>AnalyticsPage</div>;
 }
+
+export const dynamic = "force-dynamic";
